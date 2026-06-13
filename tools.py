083b2e82744d65e3d74f2626a873a038379d274e
Sapto Mukti Handoyo <saptomh@gmail.com>
2026-06-13 18:48:49 +0700

    Delete Create Retriever Tool

--- tools.py
+++ tools.py
@@ -14,7 +14,6 @@ from langchain_community.vectorstores import FAISS
 from langchain_community.document_loaders import DirectoryLoader, TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_google_genai import GoogleGenerativeAIEmbeddings
-from langchain_core.tools.retriever import create_retriever_tool
 
 # =========================================================
 # Koordinat basecamp gunung (untuk fitur cek cuaca)
